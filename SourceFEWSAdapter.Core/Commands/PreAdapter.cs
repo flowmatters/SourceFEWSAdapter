5cfd73ef3d8d6b6a24299e1e507db1c1c0b72206
Joel Rahman <joel@flowmatters.com.au>
2024-12-09 11:30:37 +1100

    Reinstate CsvHelper, needed by IO libraries

--- SourceFEWSAdapter.Core/Commands/PreAdapter.cs
+++ SourceFEWSAdapter.Core/Commands/PreAdapter.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using Microsoft.VisualBasic.FileIO;
+using CsvHelper;
 using SourceFEWSAdapter.Core;
 using SourceFEWSAdapter.FEWSPI;
 using TIME.DataTypes;
